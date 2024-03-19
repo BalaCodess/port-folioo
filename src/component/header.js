f76944ccabcd76978e7e7da7e3491177d664ef5d
BalaCodess <athibala046@gmail.com>
2024-03-19 15:30:32 +0530

    nitify c

--- src/component/header.js
+++ src/component/header.js
@@ -4,10 +4,10 @@ import { useState } from 'react'
 export default function Header(){
     const [toggleMenu,setToggleMenu]=useState(false);
   return <header className="flex justify-between px-2 bg-primary h-[100px]  text-center sticky">
-    <a className="font-bold text-white py-10 text-4xl" href="" >Portfolio</a>
+    <a className="font-bold text-white py-10 text-4xl" href="www.google.com" >Portfolio</a>
     <nav className="hidden md:block " >
     <ul className="flex text-white py-10 text-2xl ">
-        <li><a>Home</a></li>
+        <li><a href="#Home">Home</a></li>
         <li><a href="#about" rel="noopener">About</a></li>
         <li><a href="#projects" rel="noopener"> Projects</a></li>
         <li><a href="#resume" rel="noopener"> Resume</a></li>
@@ -16,7 +16,7 @@ export default function Header(){
     </nav>
     {toggleMenu && <nav className='block md:hidden '>
     <ul onClick={()=>setToggleMenu(!toggleMenu)} className="flex flex-col text-white mobile-nav">
-        <li><a >Home</a></li>
+        <li><a href="#Home">Home</a></li>
         <li><a href="#about" rel="noopener">About</a></li>
         <li><a href="#projects" rel="noopener" > Projects</a></li>
         <li><a href="#contact" rel="noopener"> Contact</a></li>
